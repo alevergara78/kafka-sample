b45e8646035fa63b6409054f74a88df167acd90d
alevergara <alevergara78@gmail.com>
2015-06-23 19:49:20 -0400

    setup de proyecto
    Implementacion de producer para test

---
+++ src/cl/orsan/kafka/TestProducer.java
@@ -0,0 +1,26 @@
+package cl.orsan.kafka;
+
+import java.util.*;
+
+import kafka.javaapi.producer.Producer;
+import kafka.producer.KeyedMessage;
+import kafka.producer.ProducerConfig;
+ 
+public class TestProducer {
+    public static void main(String[] args) {
+     
+        Properties props = new Properties();
+        props.put("metadata.broker.list", "localhost:9092");
+        props.put("serializer.class", "kafka.serializer.StringEncoder");
+        //props.put("partitioner.class", "example.producer.SimplePartitioner");
+        props.put("request.required.acks", "1");
+ 
+        ProducerConfig config = new ProducerConfig(props);
+ 
+        Producer<String, String> producer = new Producer<String, String>(config);
+        KeyedMessage<String, String> data = new KeyedMessage<String, String>("test", "0001", "test mesage");
+        producer.send(data);
+        
+        producer.close();
+    }
+}
