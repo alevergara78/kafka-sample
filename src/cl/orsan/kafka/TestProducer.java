8c791d7cb5d778732223893d1e22ab664cc450d0
alevergara <alevergara78@gmail.com>
2015-06-24 14:19:36 -0400

    corrección en pom
    configure el pom para que convierta el jar en runnable usando el
    comando mvn package

--- src/cl/orsan/kafka/TestProducer.java
+++ src/cl/orsan/kafka/TestProducer.java
@@ -1,5 +1,7 @@
 package cl.orsan.kafka;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 import kafka.javaapi.producer.Producer;
@@ -9,6 +11,9 @@ import kafka.producer.ProducerConfig;
 public class TestProducer {
     public static void main(String[] args) {
      
+    	DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+    	Date date = new Date();
+    	
         Properties props = new Properties();
         props.put("metadata.broker.list", "localhost:9092");
         props.put("serializer.class", "kafka.serializer.StringEncoder");
@@ -18,7 +23,7 @@ public class TestProducer {
         ProducerConfig config = new ProducerConfig(props);
  
         Producer<String, String> producer = new Producer<String, String>(config);
-        KeyedMessage<String, String> data = new KeyedMessage<String, String>("test", "0001", "test mesage");
+        KeyedMessage<String, String> data = new KeyedMessage<String, String>("test", "0001", "test mesage a las: "+dateFormat.format(date));
         producer.send(data);
         
         producer.close();
